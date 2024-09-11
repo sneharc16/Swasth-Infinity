1b7b6fa55ce01e2c604b5db60f9da8fe73305a5d
Agarwalvidu <119511621+Agarwalvidu@users.noreply.github.com>
2024-09-11 15:55:09 +0530

    updated inks

--- src/components/sustain/card.js
+++ src/components/sustain/card.js
@@ -8,7 +8,7 @@ function Card() {
       <h4 className="card10-title">Carbon Footprint Counter</h4>
       <p className="card10-text">Click on the below button to checkout the daily carbon footprint generated based on various fitness activites.</p>
       <button className="card10-button">
-        <a href="https://bcf4d70d2743803310.gradio.live/" target="_blank" rel="noopener noreferrer" className="card10-link">
+        <a href="https://huggingface.co/spaces/Manishikha/CarbonFootPrintCalculator" target="_blank" rel="noopener noreferrer" className="card10-link">
           Calculate
         </a>
       </button>
@@ -17,7 +17,7 @@ function Card() {
     <h4 className="card10-title">Generate Sustain Points</h4>
     <p className="card10-text">Click on the below button to generate your daily sustain points to take one step ahed towards sustainability.</p>
     <button className="card10-button">
-      <a href="https://36d2538d93f37d6159.gradio.live/" target="_blank" rel="noopener noreferrer" className="card10-link">
+      <a href="https://huggingface.co/spaces/Manishikha/Dashboard" target="_blank" rel="noopener noreferrer" className="card10-link">
         Generate
       </a>
     </button>
